08e3b7525c10fd7998a0a069921b7ba268e92890
benjaminaronson <benjician88@gmail.com>
2023-11-10 18:42:19 -0500

    test

--- Assets/Scripts/PauseScreen.cs
+++ Assets/Scripts/PauseScreen.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PauseScreen : MonoBehaviour
 {
-    public bool isPaused = false;
+    public bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPaused = false;
     }
 
     // Update is called once per frame
@@ -16,7 +16,8 @@ public class PauseScreen : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-
+            isPaused = !isPaused;
+            Debug.Log(isPaused);
         }
     }
 }
