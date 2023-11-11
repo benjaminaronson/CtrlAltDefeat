6b0c255d102d9c8adfbc19b51e26c7ba2f2ed19c
benjaminaronson <benjician88@gmail.com>
2023-11-11 17:00:13 -0500

    added boundaries and started level3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -100,8 +100,17 @@ public class PlayerMovement : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        grounded = true;
+        
+            grounded = true;
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground") || collision.collider.GetType() == typeof(CircleCollider2D) 
+            || collision.gameObject.CompareTag("Enemy"))
+        {
+            grounded = true;
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
