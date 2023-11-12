d04b47f468a836d3dc93eb56108a524f9dbe4553
benjaminaronson <benjician88@gmail.com>
2023-11-11 20:00:44 -0500

    added a bunch of stuff

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -16,6 +16,18 @@ public class KillZone : MonoBehaviour
         
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collidng!");
+        Debug.Log(collision.gameObject.tag);
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // kill player
+            collision.gameObject.GetComponent<PlayerDeath>().die();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
